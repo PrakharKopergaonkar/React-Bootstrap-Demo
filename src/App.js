5925c77362cbc69f68f93d9dff730c9b3d04cbef
prakhar kopergaonkar <pkopergaonkar@gmail.com>
2021-01-29 16:12:01 +0530

    form update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import './App.css';
-import {Button, Alert, Breadcrumb, Card, Form} from 'react-bootstrap'
+import {Container, Row, Col, Button, Alert, Breadcrumb, Card, Form} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Container>
         <Form>
-          <Form.Group>
-            <Form.Label> Email Address </Form.Label>
-            <Form.Control type="email" placeholder="Example@email.com"/>
-            <Form.Text className="text-muted"> 
-              We will never share your email Address with anyone.
-            </Form.Text> 
-          </Form.Group>
-          <Form.Group>
-            <Form.Label> Password </Form.Label>
-            <Form.Control type="password" placeholder="Password"/>
-          </Form.Group>
+          <Row>
+            <Col md>
+              <Form.Group controlId="FormEmail">
+                <Form.Label> Email Address </Form.Label>
+                <Form.Control type="email" placeholder="Example@email.com"/>
+                <Form.Text className="text-muted"> 
+                  We will never share your email Address with anyone.
+                </Form.Text> 
+              </Form.Group>
+            </Col>
+            <Col md>
+              <Form.Group controlId="FormPassword">
+                <Form.Label> Password </Form.Label>
+                <Form.Control type="password" placeholder="Password"/>
+              </Form.Group>
+            </Col>
+          </Row>
           <Button variant="secondary" type="submit"> Login </Button>
         </Form>
         <Card className="mb-3" style={{color: "#000"}}>
-          <Card.Img src="https://picsum.photos/200/50" />
+          <Card.Img src="https://picsum.photos/400/100" />
           <Card.Body>
             <Card.Title> 
             Card Example
@@ -38,6 +45,7 @@ function App() {
         </Breadcrumb>      
         <Alert variant="success"> This is a Button</Alert>
         <Button> Test Button</Button>
+        </Container>
       </header>
     </div>
   );
